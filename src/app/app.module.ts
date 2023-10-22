aec2421d57995b1892b4ba1887568e378131e96a
Martina Mucci <martina22.12@hotmail.com>
2023-10-22 01:31:32 -0300

    started working on body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeInfoComponent } from './components/home/pages/home-info/home-info.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, HomeInfoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
